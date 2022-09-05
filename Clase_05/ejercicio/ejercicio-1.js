294b5060b633ff4e696a53f552eb6ecca31fc059
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2022-09-05 10:52:18 -0300

    Clase 05 - funciones

--- Clase_05/ejercicio/ejercicio-1.js
+++ Clase_05/ejercicio/ejercicio-1.js
@@ -6,7 +6,26 @@
 - Se debe considerar la posibilidad de que el usuario quiera ver antes de ingresar, realizar la  validación.
 
 */
+let sigla;
 
 function ingresarCarrera(){
-    console.log('Solicitando carrera');
+    do {
+        sigla =  prompt('Ingrese la carrera').toLowerCase();
+
+    } while( sigla != 'dw' && sigla != 'dm' && sigla != 'dg' );
+    
+}
+
+function mostrarCarrera(){
+    if( sigla == undefined) {
+        alert('No ingreso las siglas');
+    }
+
+    if( sigla == 'dm' ){
+        console.info('La carrera es Diseño Web');
+    } else if( sigla == 'dm'){
+        console.info('La carrera es Diseño Multimedial');
+    } else if( sigla == 'dg'){
+        console.info('La carrera es Diseño Gráfico');
+    }
 }
