9317a92bc007ef2080368f29daa0b4f14036643d
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2022-09-05 09:51:16 -0300

    Clase 05 - funciones

---
+++ Clase_05/ejercicio/ejercicio-1.js
@@ -0,0 +1,12 @@
+/*
+- El primero dice “Ingresar siglas de la carrera”, que al hacerle click una función debe  ingresar por prompt una de las siguientes siglas: dw, dm, dg.
+- El segundo dice “Mostrar carrera”, que al hacerle click una función debe mostrar  mediante console.info el nombre completo de la carrera: Diseño web, Diseño  multimedial, Diseño gráﬁco.
+
+- Se deben validar los valores ingresados.
+- Se debe considerar la posibilidad de que el usuario quiera ver antes de ingresar, realizar la  validación.
+
+*/
+
+function ingresarCarrera(){
+    console.log('Solicitando carrera');
+}
