e49115565fa792af46b73e522cdecba103986481
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-09-09 09:07:51 -0300

    Clase 06 - Funciones

--- Clase_06/ejemplos/ejemplo-1.js
+++ Clase_06/ejemplos/ejemplo-1.js
@@ -1,1 +1,11 @@
 console.log('Ejemplo 01');
+const CrearImg = function() {
+    document.write('<img src="' + rutaArchivo + '" />');
+}
+
+let rutaArchivo;
+rutaArchivo = 'img/FF0000.jpg';
+
+CrearImg();
+rutaArchivo = 'img/00FF00.jpg';
+CrearImg();
---
+++ Clase_06/ejemplos/ejemplo-2.js
@@ -0,0 +1,3 @@
+const CrearImgOpcional = function(rutaArchivo = 'img/000000.jpg') {
+    document.write(`<img src="${rutaArchivo}" alt="Imagen de un color" />`);
+}
