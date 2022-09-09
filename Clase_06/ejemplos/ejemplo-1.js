8ab49b71c78bf48738a3265a953a3d547ad868f6
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-09-09 09:41:05 -0300

    Clase 06 - Funciones

--- Clase_06/ejemplos/ejemplo-1.js
+++ Clase_06/ejemplos/ejemplo-1.js
@@ -1,11 +1,30 @@
 console.log('Ejemplo 01');
-const CrearImg = function() {
+let rutaArchivo;
+
+
+/* const CrearImg = function() {
     document.write('<img src="' + rutaArchivo + '" />');
 }
 
-let rutaArchivo;
 rutaArchivo = 'img/FF0000.jpg';
 
 CrearImg();
 rutaArchivo = 'img/00FF00.jpg';
 CrearImg();
+
+rutaArchivo = 'img/FF0000.jpg';
+
+CrearImg(); */
+
+function crearImg(ruta, titulo='Imagen de cuadrado'){
+    document.write(`<img src="${ruta}"  title="${titulo}" alt="${titulo}">`);
+}
+
+crearImg('img/FF0000.jpg', 'Cuadrado rojo');
+crearImg('otra imagen', 'img/FFFF00.jpg'); // El orden de los argumento es importante
+crearImg('img/0000FF.jpg');
+
+
+
+// ruta es una parametro de la funcion. Solo pertenece a ella
+// console.log(ruta); 
