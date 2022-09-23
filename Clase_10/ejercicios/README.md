5109d92dbe58cc35cceb8b9d0e79fbb64e4b23e8
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-09-23 15:01:19 -0300

    Clase 10 - Array multidimensionales - Ejercicio 1 Resolución

---
+++ Clase_10/ejercicios/README.md
@@ -0,0 +1,8 @@
+# Ejercicio 1
+
+Tomando como base la matriz sodaStereo:
+* Modiﬁcar las posiciones numéricas del segundo nivel por posiciones asociativas.
+* Con un for común recorrer el primer nivel.
+* Dentro del for, utilicemos un for in para ir recorriendo cada índice del segundo nivel  (utilizar un condicional para ver si es el campo correspondiente a la imagen para  mostrarlo dentro de una etiqueta img).
+* Utilizar el índice asociativo como título del campo a mostrar.
+Iremos armando todo en una variable info que luego será mostrada en el HTML.  Cada integrante en un ul, cada campo en un li, el “título” en etiquetas strong.  
--- Clase_10/ejercicios/ejercicio-1.js
+++ Clase_10/ejercicios/ejercicio-1.js
@@ -1,7 +1,5 @@
-// Variable para guardar mensaje:
-let info = '';
-
 // Matriz base:
+/*
 let sodaStereo = [];
 
 sodaStereo[0] = [];            // Necesito indicar que la posición 0 será un array.
@@ -21,3 +19,60 @@ sodaStereo[2][0] = 'Carlos';
 sodaStereo[2][1] = 'Ficicchia';
 sodaStereo[2][2] = 'Batería';
 sodaStereo[2][3] = 'soda-stereo-alberti.jpg';
+*/
+
+/* ------------------------------- 1. ------------------------------- */
+
+// Variable para guardar mensaje:
+let info = '';
+// Arrays
+let sodaStereo = [];
+let cerati = [];
+let bosio = [];
+let ficicchia = [];
+
+cerati['nombre'] = 'Gustavo';
+cerati['apellido'] = 'Cerati';
+cerati['instrumento'] = 'Guitarra';
+cerati['imagen'] = 'soda-stereo-cerati.jpg';
+
+bosio['nombre'] = 'Héctor';
+bosio['apellido'] = 'Bosio';
+bosio['instrumento'] = 'Bajo';
+bosio['imagen'] = 'soda-stereo-bosio.jpg';
+
+ficicchia['nombre'] = 'Carlos';
+ficicchia['apellido'] = 'Ficicchia';
+ficicchia['instrumento'] = 'Batería';
+ficicchia['imagen'] = 'soda-stereo-alberti.jpg';
+
+sodaStereo = [ cerati, bosio];
+sodaStereo.push(ficicchia);
+/* ----------------------------------- 2. ----------------------------------- */
+for (let i=0; i<sodaStereo.length; i++){
+    // Crea la etiqueta ul de apertura
+    info += '<ul>'; 
+    const musico = sodaStereo[i];
+    console.log(musico)
+    /* ----------------------------------- 3. ----------------------------------- */
+    for (const indice in musico) {
+        console.log(indice);
+        // Crea cada li por musico
+        if( indice == 'imagen'){
+            info += `<li>
+                        <strong>${indice}</strong>
+                        <img src="img/${musico[indice]}">
+                    </li>`;
+        } else {
+            info += `<li>
+                        <strong>${indice}</strong>
+                        ${musico[indice]}
+                    </li>`;
+        }
+    }
+    // Cierra la ul
+    info += '</ul>'; 
+}
+
+ /* -------------------------- Muestra la información ------------------------- */
+ document.getElementById('info').innerHTML = info;
